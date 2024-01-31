afe55d3b5c8bc78e85745dc850b30acecc4eaa60
rekla abderrahim <reklaabderrahim@hotmail.com>
2024-01-31 07:58:08 +0100

    Change databases,kafka endpoints

--- docker/scripts/init.sql
+++ docker/scripts/init.sql
@@ -1,5 +1,5 @@
 -- CREATE USER market_owner WITH PASSWORD 'market_owner';
 CREATE USER market_update WITH PASSWORD 'market_update';
 CREATE USER market_select WITH PASSWORD 'market_select';
--- CREATE DATABASE market;
+CREATE DATABASE market;
 GRANT ALL PRIVILEGES ON DATABASE market TO market_owner;
